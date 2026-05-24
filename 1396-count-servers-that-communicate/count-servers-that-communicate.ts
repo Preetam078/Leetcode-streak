cdda76015a64a74ba141ec54203297f16a1604b8
Preetam Mondal <81969353+Preetam078@users.noreply.github.com>
2026-05-24 15:57:29 +0530

    Time: 2 ms (88.00%) | Memory: 59.6 MB (80.00%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.ts
@@ -0,0 +1,30 @@
+function countServers(grid: number[][]): number {
+    const m = grid.length;
+    const n = grid[0].length;
+
+    const rowCount = new Array(m).fill(0);
+    const colCount = new Array(n).fill(0);
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                rowCount[i]++;
+                colCount[j]++;
+            }
+        }
+    }
+    
+    let totalServers = 0;
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                if (rowCount[i] > 1 || colCount[j] > 1) {
+                    totalServers++;
+                }
+            }
+        }
+    }
+    
+    return totalServers;
+}
