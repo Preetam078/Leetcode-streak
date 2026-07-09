4692b893465896d652cbe77d3de849b747757263
Preetam Mondal <81969353+Preetam078@users.noreply.github.com>
2026-07-10 01:01:04 +0530

    Time: 115 ms (5.26%) | Memory: 94.3 MB (5.26%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.js
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.js
@@ -6,25 +6,42 @@
  * @return {boolean[]}
  */
 var pathExistenceQueries = function(n, nums, maxDiff, queries) {
-    const groupId = Array(n).fill(0);
-    let currentGroup = 0;
-    
-    for (let i = 1; i < n; i++) {
-        if (Math.abs(nums[i] - nums[i - 1]) > maxDiff) {
-            currentGroup++;
-        }
-        groupId[i] = currentGroup;
-    }
-    
-    const ans = Array(queries.length).fill(false);
-    
+    const parent = Array.from({length: n}, (_, i) => i);
+    const size = Array(n).fill(1); 
 
-    for (let i = 0; i < queries.length; i++) {
-        const [u, v] = queries[i];
-        if (groupId[u] === groupId[v]) {
-            ans[i] = true;
+    const getParent = (n) => {
+        if(parent[n] === n) return n;
+        return parent[n] = getParent(parent[n]); // path compression
+    }
+
+    const unite = (i, j) => {
+        const rooti = getParent(i);
+        const rootj = getParent(j);
+
+        if(rooti === rootj) return false;
+        if(size[rooti] > size[rootj]) {
+            parent[rootj] = rooti;
+            size[rooti] += size[rootj]; 
+        } else {
+            parent[rooti] = rootj;
+            size[rootj] += size[rooti]; 
+        }
+        return true;
+    }
+
+    for(let i = 1; i < n; i++) {
+        if(Math.abs(nums[i] - nums[i - 1]) <= maxDiff) {
+            unite(i, i - 1);
         }
     }
-    
+
+    const ans = [];
+    for(const [src, dest] of queries) {
+        if(getParent(src) === getParent(dest)) {
+            ans.push(true);
+        } else {
+            ans.push(false);
+        }
+    }
     return ans;
 };
